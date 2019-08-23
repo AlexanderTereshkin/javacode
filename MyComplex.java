d88b6ce259168173207e3487dc37b27c32c2cf25
AlexanderTereshkin <nnovcity@mail.ru>
2019-08-24 00:41:24 +0300

    Operations with complex numbers.

--- MyComplex.java
+++ MyComplex.java
@@ -58,51 +58,74 @@ public class MyComplex {
         return ((this.real == another.getReal()) && (this.imag == another.getImag()));
     }
 
+    // Return module of complex number
     public double magnitude() {
         return Math.sqrt((this.real * this.real) + (this.imag * this.imag));
     }
 
+    // In radians
     public double argument() {
         return Math.atan(this.imag / this.real);
     }
 
+    // Return "this" object after addition (this does`t make some new object)
     public MyComplex add(MyComplex rigth) {
         this.setReal(this.real + rigth.getReal());
         this.setImag(this.imag + rigth.getImag());
         return this;
     }
 
+    // Return new object after addition
     public MyComplex addNew(MyComplex rigth) {
         MyComplex newComplex = new MyComplex(this.real + rigth.getReal(), this.imag + rigth.getImag());
         return newComplex;
     }
 
+    // Return "this" object after subtract (this does`t make some new object)
     public MyComplex subtract(MyComplex right) {
         this.setReal(this.real - right.getReal());
         this.setImag(this.imag - right.getImag());
         return this;
     }
 
+    // Return new object after subtract
     public MyComplex subtractNew(MyComplex right) {
         MyComplex newComplex = new MyComplex(this.real - right.getReal(), this.imag - right.getImag());
         return newComplex;
     }
 
+    // Return "this" object after multiply (this does`t make some new object)
     public MyComplex multiply(MyComplex right) {
         this.setValue((this.real * right.getReal() - this.imag * right.getImag()), (this.real * right.getImag() + this.imag * right.getReal()));
         return this;
     }
 
+    // Return new object after multiply
+    public MyComplex multiplyNew(MyComplex right) {
+        MyComplex newComplex = new MyComplex((this.real * right.getReal() - this.imag * right.getImag()), (this.real * right.getImag() + this.imag * right.getReal()));
+        return newComplex;
+    }
+
+    // Private multiply method for divide method. Real and imaginary parts multiplies to imag (which calculated in divide method) separatly.
     private MyComplex multiply(double imag) {
         return new MyComplex(this.real * imag, this.imag * imag);
     }
 
+    // Make conjucate complex number (new object) of denominator (Use this method for divide method)
     public MyComplex conjucate() {
         return new MyComplex(real, -imag);
     }
 
+    // Return "this" object after divide (this does`t make some new object)
     public MyComplex divide(MyComplex right) {
-        double imag = 1 / (right.getReal() * right.getReal() + right.getImag() * right.getImag());  // + за счёт мнимой единицы
+        double imag = 1 / (right.getReal() * right.getReal() + right.getImag() * right.getImag());
         return this.multiply(right.conjucate()).multiply(imag);
+    }
+
+    // Return new object after divide
+    public MyComplex divideNew(MyComplex right) {
+        double imag = 1 / (right.getReal() * right.getReal() + right.getImag() * right.getImag());
+        MyComplex newComplex = this.multiplyNew(right.conjucate()).multiply(imag);
+        return newComplex;
     }
 }
--- Task1.java
+++ Task1.java
@@ -4,9 +4,25 @@ public class Task1 {
     public static void main(String[] args) {
         MyComplex mycomplex1 = new MyComplex(3,1);
         MyComplex mycomplex2 = new MyComplex(5,-2);
-        System.out.println(mycomplex1.addNew(mycomplex2));
-        System.out.println(mycomplex1.subtractNew(mycomplex2));
-        System.out.println(mycomplex1.multiply(mycomplex2));
-        System.out.println(mycomplex1.divide(mycomplex2));
+        System.out.println("Complex number mycomplex1 " + mycomplex1);
+        System.out.println("Complex number mycomplex2 " + mycomplex2);
+        System.out.println("Result of addition " + mycomplex1 + " and " + mycomplex2 + " = " + mycomplex1.addNew(mycomplex2));
+        System.out.println("Result of subtract " + mycomplex1 + " and " + mycomplex2 + " = " + mycomplex1.subtractNew(mycomplex2));
+        System.out.println("Result of multiply " + mycomplex1 + " and " + mycomplex2 + " = " + mycomplex1.multiplyNew(mycomplex2));
+        System.out.println("Result of divide " + mycomplex1 + " and " + mycomplex2 + " = " + mycomplex1.divideNew(mycomplex2));
+
+        /**
+         * Let`s check for divide of complex numbers.
+         * If newMyComplex = mycomplex1 / mycomplex2
+         * So mycomplex1 must be equals to newMyComplex2 = (newMyComplex * mycomplex2)
+         */
+        MyComplex newMyComplex = mycomplex1.divideNew(mycomplex2);
+        MyComplex newMyComplex2 = newMyComplex.multiplyNew(mycomplex2);
+        System.out.println("After multiply, this result of divide, with mycomplex2, we have: " + newMyComplex2);
+        if (newMyComplex2.equals(mycomplex1)) {
+            System.out.println("This is equal to mycomplex1 " + mycomplex1);
+        } else {
+            System.out.println("This is NOT equal to mycomplex1 " + mycomplex1);
+        }
     }
 }
